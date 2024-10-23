4feef43c38a3ea767145ec08211baec1d07e1564
lsy28901 <tndus28901@gmail.com>
2024-10-23 17:52:17 +0900

    빠르게 소켓 테스트용 수정

---
+++ src/main/java/com/chat/websocket/Message.java
@@ -0,0 +1,12 @@
+package com.chat.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String content;
+}
--- src/main/java/com/chat/websocket/WebSocket.java
+++ src/main/java/com/chat/websocket/WebSocket.java
@@ -1,4 +1,14 @@
 package com.chat.websocket;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class WebSocket {
+    @MessageMapping("/send")
+    @SendTo("/topic/test")
+    public Message broadCast(Message message){
+        return message;
+    }
 }
