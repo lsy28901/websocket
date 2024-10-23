ead06c5d09f0d5582e58480418307967ef9e0167
lsy28901 <tndus28901@gmail.com>
2024-10-23 18:46:35 +0900

    빠르게 소켓 테스트용 수정

--- src/main/java/com/chat/websocket/WebSocket.java
+++ src/main/java/com/chat/websocket/WebSocket.java
@@ -1,14 +1,17 @@
 package com.chat.websocket;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 public class WebSocket {
     @MessageMapping("/send")
     @SendTo("/topic/test")
     public Message broadCast(Message message){
+        log.info("메시지를 받았습니다 ={}",message);
         return message;
     }
 }
